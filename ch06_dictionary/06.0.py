f98126add91b78b823da780b678028f5d6d2af90
kingboseong <rlaqhtjd8001@naver.com>
2024-03-13 11:24:24 +0900

    03_13

--- ch06_dictionary/06.0.py
+++ ch06_dictionary/06.0.py
@@ -15,6 +15,28 @@ family = {
     'daddy':'blue',
     'mammy':'pink',
     'me':'red',
-    'bro':'skyblue',
+    'bro':'blue',
 }
 print(f'엄마가 좋아하는 색깔은 {family["mammy"]}입니다.')
+#키-값 쌍 순회
+for k,v in family.items():
+    print(f'{k}가 좋아하는 색깔은 {v}입니다.')
+#key정렬 = .key(): key 출력은 기본제공이라  .key()는 생략해도 됨.
+for k in sorted(family):
+    print(k)
+#값 순회
+for v in family.values():
+    print(f'색 종류 : {v}')
+#중복제거
+for v in set(family.values()):
+    print(v)
+
+# alines = []
+# for alien_number in range(10):
+#     new_alien = {'color':'green','point':5,'speed':'slow'}
+#     alines.append(new_alien)
+# for alien in alines[:5]:
+#     print(aline)
+# print('...')
+# print(f'Total number of aliens: {len(alines)}')
+
--- ch12_ship/ship.py
+++ ch12_ship/ship.py
@@ -1,7 +1,10 @@
-
-import sys
 import pygame
+import sys
 
+# import setting
+# from util import init, create_bullet, hendle_key_event,
+
+# screen, clock, image, screen_rect, ship_rect,
 pygame.init()
 
 screen = pygame.display.set_mode((1280,720))
@@ -12,6 +15,18 @@ image = pygame.image.load('./pcc_3e/chapter_12/adding_ship_image/images/ship.bmp
 
 rect = image.get_rect()
 
+rect.midbottom = screen.get_rect().midbottom
+
+def create_bullet(rect):
+    bullet = pygame.Rect(0,0,5,50)
+    bullet.midtop - rect.midtop
+    bullet.top -= rect.height
+    bullet_color = (255,0,0)
+    return bullet,bullet_color
+
+bullet = create_bullet(rect)
+
+
 while True:
     # Process player inputs.
     for event in pygame.event.get():
@@ -19,6 +34,15 @@ while True:
             pygame.quit()
             sys.exit
             # raise SystemExit
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                rect.left -= 10
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RIGHT:
+                rect.left += 10
+        
+
+        
 
 #키보드 이벤트 동작 구현 / updates위치 -비행기의
     # Do logical updates here.
@@ -31,6 +55,7 @@ while True:
     # Render the graphics here.
     # ...
     screen.blit(image, rect)
+    pygame.draw.rect(screen, bullet)
 
 #flip() = 다시 while문으로 이동해서 계속 돈다. => 이를 통해서 비행기가 이동하게 만들어 준다. => 60fps형성됨.
 #이동한 비행기의 모습을 그리고 flip해주는 것을 초당 60번씩 실행한다.
